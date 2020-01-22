b988a8fc399bd61dd94062ee40f4011f62f4fae9
Firuz Afzalov <afzalov98@gmail.com>
2020-01-22 02:05:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,49 @@
-def reverse_each_word(sentence1)
+def reverse_each_word()
+original_string = string.split(" ")
+arr = []
+original_string.each do |str|
+   arr << str.reverse
   
-  # %w[sentence1].map{ |item| puts item.reverse }
-
-  
-  p sentence1
-  
+  end 
+  arr.join(" ")
 end 
+reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(string)
+  original_string = string.split(" ")
+  arr = []
+  original_string.collect do |str| 
+    arr << str.reverse
+    end 
+    arr.join(" ")
+end 
+reverse_each_word("Hello there, and how are you?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
